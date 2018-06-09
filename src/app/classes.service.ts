0020e0c63828206ecdc8133e27b4e05b28a76931
kevin-ziteng <kevin.ztwang@gmail.com>
2018-06-09 08:55:00 -0400

    define service

--- src/app/classes.service.ts
+++ src/app/classes.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Class } from './class';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassesService {
+
+  classes: Class[] = [
+    {
+      id: 1,
+      code: 'Http5301',
+      name: 'Usability and Accessibility',
+      instructor: 'Simon Borer'
+    },
+    {
+      id: 2,
+      code: 'Http5302',
+      name: 'Trends in Web Development',
+      instructor: 'Lee Situ'
+    },
+    {
+      id: 3,
+      code: 'Http5303',
+      name: 'Web Project',
+      instructor: 'Nithya Thayananthan'
+    },
+    {
+      id: 4,
+      code: 'Http5304',
+      name: 'Portfolio Development',
+      instructor: 'Joanna Kommala'
+    }
+  ];
+
+  getClasses(): Observable<Class[]>{
+    return of(this.classes);
+  }
 
   constructor() { }
 }
