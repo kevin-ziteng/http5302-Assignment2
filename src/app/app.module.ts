d15c1008c5a6804f89ac5248537297a1fe8a1131
kevin-ziteng <kevin.ztwang@gmail.com>
2018-06-14 14:11:43 -0400

    Kevin: part2 api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TimetableComponent } from './timetable/timetable.component';
@@ -10,7 +11,8 @@ import { TimetableComponent } from './timetable/timetable.component';
     TimetableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/classes.service.ts
+++ src/app/classes.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Class } from './class';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassesService {
+
+  private handleError(error: HttpErrorResponse){
+    if (error.error instanceof ErrorEvent){
+      console.error('An error occurred: ', error.error.message);
+    } else{
+      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    return throwError('Something bad happened!');
+  }
 
   classes: Class[] = [
     {
@@ -39,8 +50,13 @@ export class ClassesService {
   ];
 
   getClasses(): Observable<Class[]>{
-    return of(this.classes);
+    // return of(this.classes);
+    return this.http.get<Class[]>('http://wangziteng.com/api.php?api_key=testkey2').pipe(
+      catchError(this.handleError)
+    );
   }
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 }
